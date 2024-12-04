297d103c9ed5bab714c40937b23120023be886ac
IgnacioBVergara <ig.bello@duocuc.cl>
2024-12-04 12:28:49 -0300

    a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'proyecto_ionic2',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    'BarcodeScanner': {
+      // Aquí puedes agregar configuraciones personalizadas si es necesario
+      // Por ejemplo, podrías añadir configuraciones como:
+      // "scanArea": "50%",
+      // "scanDelay": 1000,
+    },
+  },
 };
 
 export default config;
